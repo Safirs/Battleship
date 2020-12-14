c8eb7eb39ac5b086df48476a173522f515b35f50
Safirs <strode.eliza@yahoo.com>
2020-12-14 19:15:03 +0200

    Pseudocode

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //PSEUDOCODE
+
+        //1. Must make an array with rows and collums 10x10
+
+         //  *   Maybe at the beginig lets print out basic rules of the game?
+                // How many ships, what is the purpose of the game ... ?
+        //2. how to randomaze the ship positions every time?
+        //3. user input with ship arrangement (how many ships?)
+                    // 1 x carrier (lenght 5)
+                    // 2 x Battle ship (lenght 4)
+                    //  3  x Cruiser (lenght 3 )
+                    // 4 x  Submariine ( lenght 2 )
+                    // 5 x Destroyer (lenght 1)
+
+        //4. player enters (x,y)
+                // if ship drowned then
+                    // must be printed out
+        //5. computers turn (x,y)
+            // if computer drowned something from user
+                // it must be printed out + the map must be printed out as well
+
+
+
+
+	// write your code here
+    }
+}
